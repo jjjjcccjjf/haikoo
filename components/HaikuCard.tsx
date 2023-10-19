d7f74d7215e783c5174bfce2e2b612e9d89d930f
Ro11ingButler <rollingkai1214@gmail.com>
2023-10-19 19:17:31 +0800

    Add pfp for HaikuCard

--- components/HaikuCard.tsx
+++ components/HaikuCard.tsx
@@ -1,20 +1,28 @@
+import Image from "next/image";
 import React from "react";
-import { BsPostageHeart, BsFillPostageHeartFill } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+import endan from '@/app/endan.jpg'
 
 export default function HaikuCard() {
   return (
-    <div className="flex h-48 flex-auto basis-72 flex-col justify-between rounded-2xl bg-violet-300 px-8 py-4">
-      <p>
-        agitated ghosts<br></br>
-        manufactured by the mind<br></br>
-        hallucinations
-      </p>
-      <div className="flex flex-col gap-3">
-        <p>#test #hello #world</p>
-        <div>
-          <button className=" border border-yellow-50">
-            <BsPostageHeart size={24} />
-          </button>
+    <div className=" h-auto flex-auto basis-80 rounded-2xl bg-violet-300 px-8 py-4 flex-row flex gap-4">
+      <div className="relative h-12 w-12">
+        <Image src={endan} fill alt="..." className=" rounded-full"></Image>
+      </div>
+      <div className="flex  flex-col justify-between">
+        <p>@haikoo</p>
+        <p className="mt-2">
+          agitated ghosts<br></br>
+          manufactured by the mind<br></br>
+          hallucinations
+        </p>
+        <div className="mt-4 flex flex-col gap-2">
+          <p>#test #hello #world</p>
+          <div>
+            <button className=" border border-yellow-50">
+              <AiOutlineHeart size={24} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
