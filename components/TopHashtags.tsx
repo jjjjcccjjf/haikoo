69e0c4260a125eaa6553343dfd230f01c004a011
Ro11ingButler <rollingkai1214@gmail.com>
2023-10-25 04:15:10 +0800

    Add new top hashtags

--- components/TopHashtags.tsx
+++ components/TopHashtags.tsx
@@ -1,25 +1,80 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
 
 export default function TopHashtags() {
   return (
-    <div className="flex h-full w-full flex-col gap-2 divide-y divide-black rounded-2xl p-4 dark:bg-slate-800 dark:text-white">
-      <p className="text-2xl">Top Hashtags</p>
-      <div className="grid grid-flow-row grid-cols-2 gap-4 py-4 text-2xl">
-        <p className="truncate">#mood</p>
-        <p className="truncate">#nature</p>
-        <p className="truncate">#life</p>
-        <p className="truncate">#random</p>
-        <p className="truncate">#anon</p>
-        <p className="truncate">#modern</p>
-        <p className="truncate">#contemporary</p>
-        <p className="truncate">#daily</p>
-        <p className="truncate">#mood</p>
-        <p className="truncate">#nature</p>
-        <p className="truncate">#life</p>
-        <p className="truncate">#random</p>
-        <p className="truncate">#anon</p>
-        <p className="truncate">#modern</p>
-      </div>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Top Hashtags</CardTitle>
+        <CardDescription className="truncate">
+          The top #hashtags of all time
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-flow-row grid-cols-2 gap-4 py-4 ">
+          <Link
+            href="/"
+            className="truncate text-primary underline-offset-4 hover:underline "
+          >
+            #mood
+          </Link>
+          <Link
+            href="/"
+            className="truncate text-primary underline-offset-4 hover:underline "
+          >
+            #contemporarixepliasadkljkasd
+          </Link>
+          <Link
+            href="/"
+            className="truncate text-primary underline-offset-4 hover:underline "
+          >
+            #washingmachine
+          </Link>
+          <Link
+            href="/"
+            className="truncate text-primary underline-offset-4 hover:underline "
+          >
+            #contemporarixepliasadkljkasd
+          </Link>
+          <Link
+            href="/"
+            className="truncate text-primary underline-offset-4 hover:underline "
+          >
+            #contemporarixepliasadkljkasd
+          </Link>
+          <Link
+            href="/"
+            className="truncate text-primary underline-offset-4 hover:underline "
+          >
+            #contemporarixepliasadkljkasd
+          </Link>
+          <Link
+            href="/"
+            className="truncate text-primary underline-offset-4 hover:underline "
+          >
+            #contemporarixepliasadkljkasd
+          </Link>
+          <Link
+            href="/"
+            className="truncate text-primary underline-offset-4 hover:underline "
+          >
+            #contemporarixepliasadkljkasd
+          </Link>
+        </div>
+      </CardContent>
+      {/* <CardFooter>
+        <div className="grid grid-flow-row justify-items-center gap-8 py-4">
+          <Button variant={"link"} className=" text-2xl" asChild>
+            <Link href="/">#anonymous</Link>
+          </Button>
+        </div>
+      </CardFooter> */}
+    </Card>
   );
 }
